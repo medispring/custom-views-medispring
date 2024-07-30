044a44a8d81a43c5f5fb00f1e061ef410fa20e29
Maxime Mennechet <maxime.mennechet@gmail.com>
2024-07-30 17:02:02 +0200

    Add by_hcparty_mhc view in patient, this view must return all patients with startOfCoverage and no endOfCoverage

---
+++ views/patient/by_hcparty_mhc.js
@@ -0,0 +1,20 @@
+map = function (doc) {
+  var emit_patients_by_mhc = function (hcparty, doc) {
+    if(doc.medicalHouseContracts.length){
+      doc.medicalHouseContracts.forEach(function (mhc) {
+        if (mhc.startOfCoverage && (!mhc.endOfCoverage || mhc.endOfCoverage === 0 || mhc.endOfCoverage === null)) {
+          emit([hcparty, mhc.startOfCoverage, mhc.endOfCoverage, doc._id])
+        }
+      });
+    }
+
+  };
+
+  if (doc.java_type === 'org.taktik.icure.entities.Patient' && !doc.deleted) {
+    if (doc.delegations) {
+      Object.keys(doc.delegations).forEach(function (k) {
+        emit_patients_by_mhc(k, doc);
+      });
+    }
+  }
+};
