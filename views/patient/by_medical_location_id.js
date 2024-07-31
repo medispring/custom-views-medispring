baa3df69ae3ec8a01d456fd59ad3a6427aadcf69
Maxime Mennechet <maxime.mennechet@gmail.com>
2024-07-31 09:28:23 +0200

    Fixed by_medical_location_id view

--- views/patient/by_medical_location_id.js
+++ views/patient/by_medical_location_id.js
@@ -1,5 +1,5 @@
 map = function(doc) {
   if (doc.java_type == 'org.taktik.icure.entities.Patient' && !doc.deleted && doc.medicalLocationId) {
-    emit(doc.medicalLocationId, doc._id);
+    emit(doc.medicalLocationId, null);
   }
 }
